8edf4404ba9e8e2b4561bdd2cc720c08afe5127e
Rastaar Haghi <rastaarhaghi@gmail.com>
2024-11-24 21:48:59 -0800

    Initial Commit to fork

--- homeworks_rust/homeworks/homework6/enums/enums2.rs
+++ homeworks_rust/homeworks/homework6/enums/enums2.rs
@@ -1,11 +1,14 @@
 // enums2.rs
 // Make me compile! Execute `rustlings hint enums2` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move{x: i32, y: i32},
+    Echo{x: String},
+    ChangeColor{r: i32, g: i32, b: i32},
+    Quit,
+
+
 }
 
 impl Message {
@@ -17,8 +20,8 @@ impl Message {
 fn main() {
     let messages = [
         Message::Move { x: 10, y: 30 },
-        Message::Echo(String::from("hello world")),
-        Message::ChangeColor(200, 255, 255),
+        Message::Echo{x: String::from("hello world")},
+        Message::ChangeColor{r: 200, g: 255, b: 255},
         Message::Quit,
     ];
 
--- homeworks_rust/homeworks/homework6/option/option3.rs
+++ homeworks_rust/homeworks/homework6/option/option3.rs
@@ -1,8 +1,6 @@
 // option3.rs
 // Make me compile! Execute `rustlings hint option3` for hints
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -11,7 +9,8 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    //match &y: Using &y instead of y means we are borrowing y rather than moving it. This way, the value remains available after the match expression.
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
